afa614b0f1e81af632784ed2274717e06a58c4d0
Freyja Ariadne <freyja.adne@gmail.com>
2023-11-20 20:23:32 -0600

    asd
    asd

--- src/main/java/com/hivemind/hivemindremixclient/FileClient.java
+++ src/main/java/com/hivemind/hivemindremixclient/FileClient.java
@@ -2,19 +2,17 @@ package com.hivemind.hivemindremixclient;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class FileClient extends Application {
+
+	private static final String SERVER_IP = "192.168.68.103";
+	private static final int SERVER_PORT = 8080;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -24,48 +22,60 @@ public class FileClient extends Application {
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("File Client");
 
-		TextField ipTextField = new TextField("127.0.0.1");
-		TextField portTextField = new TextField("8080");
-		Button connectButton = new Button("Connect and Receive File");
-
-		connectButton.setOnAction(e -> {
-			String ipAddress = ipTextField.getText();
-			int port = Integer.parseInt(portTextField.getText());
-
-			receiveFile(ipAddress, port);
-		});
+		Label statusLabel = new Label("Waiting for files...");
 
 		VBox vbox = new VBox(10);
-		vbox.getChildren().addAll(new Label("IP Address:"), ipTextField, new Label("Port:"), portTextField, connectButton);
+		vbox.getChildren().addAll(statusLabel);
 
 		Scene scene = new Scene(vbox, 300, 150);
 		primaryStage.setScene(scene);
 
 		primaryStage.show();
-	}
 
-	private void receiveFile(String ipAddress, int port) {
+		// Start the continuous file receiving loop in a separate thread
 		new Thread(() -> {
-			try (Socket socket = new Socket(ipAddress, port);
-				 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-
-				// Read the file name from the server
-				String receivedFileName = in.readLine();
-				System.out.println("Receiving file: " + receivedFileName);
-
-				try (PrintWriter writer = new PrintWriter(receivedFileName)) {
-					String line;
-					while ((line = in.readLine()) != null) {
-						writer.println(line);
-					}
+			while (true) {
+				receiveFile(SERVER_IP, SERVER_PORT, statusLabel);
+				try {
+					// Sleep for a while before checking for new files again
+					Thread.sleep(5000); // 5 seconds (adjust as needed)
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
-
-				System.out.println("File received and saved as " + receivedFileName);
-
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
 		}).start();
 	}
 
+	private void receiveFile(String ipAddress, int port, Label statusLabel) {
+		try (Socket socket = new Socket(ipAddress, port);
+			 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+			// Read the file name from the server
+			String receivedFileName = in.readLine();
+			if (receivedFileName != null) {
+				System.out.println("Receiving file: " + receivedFileName);
+
+				try (PrintWriter writer = new PrintWriter(receivedFileName);
+					 BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+
+					String line;
+					while ((line = in.readLine()) != null) {
+						bufferedWriter.write(line);
+						bufferedWriter.newLine();
+					}
+
+					System.out.println("File received and saved as " + receivedFileName);
+
+					// Update UI or perform any other actions as needed
+					statusLabel.setText("Received file: " + receivedFileName);
+				}
+			} else {
+				// No new files at the moment
+				System.out.println("No new files.");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
